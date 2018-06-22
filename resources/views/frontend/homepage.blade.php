e00840481945791809ef2bb7b68abe5538f55999
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2018-06-22 10:49:06 -0400

    Updates

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -4,10 +4,3 @@
     @include('frontend.partials.search')
 @endsection
 
-@section('content_top')
-    @if($event)
-    <div class="bg-light-blue text-center p-2 display-6 text-uppercase">
-        <strong>{{ __('NEXT EVENT') }}:</strong> {{ Jenssegers\Date\Date::parse($event->start_at)->format('l j F Y') }}
-    </div>
-    @endif
-@endsection
