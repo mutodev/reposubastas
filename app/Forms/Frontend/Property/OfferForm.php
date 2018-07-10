9a2faea3c0977dc97b6ae5c5e82e28ddae376195
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2018-07-10 16:27:11 -0400

    Updates

--- app/Forms/Frontend/Property/OfferForm.php
+++ app/Forms/Frontend/Property/OfferForm.php
@@ -9,9 +9,8 @@ class OfferForm extends Form
     public function buildForm()
     {
         $this->add('offer', 'number', ['label' => false, 'rules' => 'required']);
-        $this->add('accept_terms', 'accept', ['value' => 1, 'rules' => 'accepted', 'label' => __('I agree to REPOSUBASTA :terms and :policy', [
-            'terms' => '<a href="'.route('frontend.page', ['locale' => \App::getLocale(), 'pageSlug' => 'terms']).'">'.__('Terms').'</a>',
-            'policy' => '<a href="'.route('frontend.page', ['locale' => \App::getLocale(), 'pageSlug' => 'policy']).'">'.__('Privacy Policy').'</a>'
+        $this->add('accept_terms', 'accept', ['value' => 1, 'rules' => 'accepted', 'label' => __('I agree to REPOSUBASTA :terms', [
+            'terms' => '<a href="'.route('frontend.page', ['locale' => \App::getLocale(), 'pageSlug' => 'terms']).'">'.__('Terms').'</a>'
         ])]);
         $this->add('submit', 'submit', ['label' => __('Submit Offer'), 'attr' => [
             'class' => 'form-control bg-light-blue border-0'
