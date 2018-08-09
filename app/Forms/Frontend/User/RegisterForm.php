e9b4e2ba7b1670c7ca0c5d80a6e03412ed991789
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2018-08-09 16:05:38 -0400

    Updates

--- app/Forms/Frontend/User/RegisterForm.php
+++ app/Forms/Frontend/User/RegisterForm.php
@@ -20,8 +20,8 @@ class RegisterForm extends Form
         $this
             ->add('name', 'text', ['label' => __('Name'), 'rules' => 'required'])
             ->add('email', 'text', ['label' => __('Email'), 'rules' => "{$required}string|email|max:255{$emailUnique}"])
-            ->add('password', 'password', ['label' => __('Password'), 'rules' => "{$required}string|min:6|confirmed"])
-            ->add('password_confirmation', 'password', ['label' => __('Password Confirmation'), 'rules' => "{$required}"])
+            ->add('password', 'password', ['label' => __('Password'), 'rules' => "{$required}string|min:6"])
+//            ->add('password_confirmation', 'password', ['label' => __('Password Confirmation'), 'rules' => "{$required}"])
             ->add('martial_status', 'select', [
                 'rules' => 'required',
                 'choices' => [
@@ -48,9 +48,9 @@ class RegisterForm extends Form
             ->add('phone2', 'text', ['label' => __('Broker Phone')]);
 
         if (!$isBackend) {
-            $this->add('captcha', 'captcha', ['label' => __('Verification code'), 'rules' => 'required|captcha', 'error_messages' => [
-                'captcha.captcha' => __('Invalid')
-            ]]);
+//            $this->add('captcha', 'captcha', ['label' => __('Verification code'), 'rules' => 'required|captcha', 'error_messages' => [
+//                'captcha.captcha' => __('Invalid')
+//            ]]);
             $this->add('accept_terms', 'accept', ['value' => 1, 'rules' => 'accepted', 'label' => __('I agree to REPOSUBASTA :terms', [
                 'terms' => '<a href="'.route('frontend.page', ['locale' => \App::getLocale(), 'pageSlug' => 'terms']).'">'.__('Terms and Conditions').'</a>',
                 'policy' => '<a href="'.route('frontend.page', ['locale' => \App::getLocale(), 'pageSlug' => 'policy']).'">'.__('Privacy Policy').'</a>'
