f7915f164d9ed118b8134941f1725901250907d3
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2019-06-27 08:45:00 -0400

    Removed events

--- resources/views/frontend/partials/paypal.blade.php
+++ resources/views/frontend/partials/paypal.blade.php
@@ -3,8 +3,6 @@
         currency="USD"
         :client="credentials"
         :button-style='{"label":"checkout",size:"small",shape: "rect",color: "gold"}'
-        v-on:payment-authorized="paymentAuthorized"
         v-on:payment-completed="paymentCompleted"
-        v-on:payment-cancelled="paymentCancelled"
 >
 </paypal>
