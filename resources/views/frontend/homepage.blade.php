ffaaffb3609d43ffb0a73c9062fa883db726afee
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2019-10-11 12:24:35 -0400

    Design

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -1,34 +1,77 @@
 @extends('frontend.base')
 
 @section('sub_header')
-    <div class="properties-search position-relative text-center p-5">
+    <div class="homepage-top position-relative p-5">
+        <div class="container">
+            <h1 class="homeText">{{ __('THE') }}<br /> <strong>{{ __('BIGGEST AUCTION') }} </strong><br />{{ __('OF THE YEAR') }}</h1>
+            <span>{{ __('NOVEMBER') }} 23, 2019 | VIVO BEACH CLUB</span><br />
+            <a class="btn btn-primary mt-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'page' => 'register']) }}">{{ __('Register now!') }}</a>
+            <a class="btn btn-outline-secondary ml-2 mt-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'page' => 'properties']) }}">{{ __('View properties') }}</a>
+        </div>
+    </div>
+
+    <div>
+        <div class="container position-relative text-center">
+            <div class="box-top-left"></div>
+            <div class="box-top-right"></div>
+            <div class="box-bottom-left"></div>
+            <div class="box-bottom-right"></div>
+            <img style="position: absolute;top: -125px;left: 50%;margin-left: -50px; z-index: 2000" src="/images/logo-icon.svg" width="100" />
+
+            <div style="position: relative; z-index: 99999999">
+                <div class="display-6 text-uppercase font-weight-bold">{{ __('WELCOME') }}</div>
+
+                <p class="mt-3">{{ __('Reposubasta welcomes you to new business opportunities where your smart investment is our priority. Each event offers you commercial, residential and land properties with the best discounts on the market. Participate in the experience and start your financial independence with solidity.') }}</p>
+
+                <br /><br /><a href="{{ route('frontend.page', ['locale' => App::getLocale(), 'page' => 'terms']) }}">Términos y Condiciones</a>
+            </div>
+        </div>
+    </div>
+
+{{--    <div class="homepage-bottom text-center position-relative p-5">--}}
+{{--        <div class="container position-relative" style="margin-top: 100px !important;">--}}
+{{--            <h1 class="display-6 text-uppercase font-weight-bold">{{ __('PARTICIPATING IN REPOSUBASTA IS EASY') }}<br /> {{ __('You just have to follow the steps:') }}</h1>--}}
+
+{{--            <div class="btn-group btn-group-toggle mt-3 steps-control" data-toggle="buttons">--}}
+{{--                <button type="button" class="btn btn-primary active" data-toggle="button" aria-pressed="false">--}}
+{{--                    {{ __('Live Auction') }}--}}
+{{--                </button>--}}
+{{--                <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false">--}}
+{{--                    {{ __('Online Auction') }}--}}
+{{--                </button>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+
+{{--    <div class="properties-search position-relative text-center p-5">--}}
 {{--        <h1 class="homeText">{{ __('NOW THE OPPORTUNITY IS AT REACH OF A CLICK') }}</h1>--}}
 
 {{--        <a href="/{{ App::getLocale() }}/properties?type=&keywords=&event_type=ONLINE&price_min=0&price_max=9999999" class="icono-home col-xs-12"></a>--}}
 
-        <table width="100%">
-            <tr>
-                <td>
-                    <div class="col-md-5 mx-auto">
-                        <div class="properties-search-box">
-                            <form method="get" action="{{ route('frontend.page', ['locale' => App::getLocale(), 'page' => 'properties']) }}">
-                                <div class="input-group mr-sm-2">
-                                    <select name="type" class="custom-select">
-                                        @foreach(App\Models\PropertyType::forSelect('All Properties') as $value => $label)
-                                            <option @if($value == request()->get('type')) selected @endif value="{{ $value }}">{{ $label }}</option>
-                                        @endforeach
-                                    </select>
-                                    <input autocomplete="off" value="{{ request()->get('keywords') }}" name="keywords" type="text" class="form-control w-50" id="keywords" placeholder="{{ __('Address, region, city, Property ID') }}">
-                                    <div class="input-group-append">
-                                        <button type="submit" class="btn btn-primary bg-light-red border-0">{{ __('Search') }}</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        </table>
-    </div>
+{{--        <table width="100%">--}}
+{{--            <tr>--}}
+{{--                <td>--}}
+{{--                    <div class="col-md-5 mx-auto">--}}
+{{--                        <div class="properties-search-box">--}}
+{{--                            <form method="get" action="{{ route('frontend.page', ['locale' => App::getLocale(), 'page' => 'properties']) }}">--}}
+{{--                                <div class="input-group mr-sm-2">--}}
+{{--                                    <select name="type" class="custom-select">--}}
+{{--                                        @foreach(App\Models\PropertyType::forSelect('All Properties') as $value => $label)--}}
+{{--                                            <option @if($value == request()->get('type')) selected @endif value="{{ $value }}">{{ $label }}</option>--}}
+{{--                                        @endforeach--}}
+{{--                                    </select>--}}
+{{--                                    <input autocomplete="off" value="{{ request()->get('keywords') }}" name="keywords" type="text" class="form-control w-50" id="keywords" placeholder="{{ __('Address, region, city, Property ID') }}">--}}
+{{--                                    <div class="input-group-append">--}}
+{{--                                        <button type="submit" class="btn btn-primary bg-light-red border-0">{{ __('Search') }}</button>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </td>--}}
+{{--            </tr>--}}
+{{--        </table>--}}
+{{--    </div>--}}
+
 @endsection
 
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.base')
 
 @section('stylesheets')
-    <link href="{{ asset('css/frontend.css', false) }}?v6" rel="stylesheet">
+    <link href="{{ asset('css/frontend.css', false) }}?v7" rel="stylesheet">
 @endsection
 
 @section('main')
-    <nav class="site-header sticky-top py-1">
+    <nav class="site-header py-1">
         <div class="container d-flex flex-column flex-md-row justify-content-between">
             <a class="py-2" href="{{ route('frontend.page', ['locale' => App::getLocale()]) }}">
-                <img src="{{ asset('images/logo.png', false) }}" height="45" />
+                <div class="main-logo"></div>
             </a>
             <div class="float-right">
-                <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'properties']) }}">{{ __('Properties') }}</a>
+{{--                <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'properties']) }}">{{ __('Properties') }}</a>--}}
                 <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'contact']) }}">{{ __('Contact Us') }}</a>
 
                 @if (Auth::guest())
-                    <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'register']) }}"><strong class="text-danger">{{ __('Sign Up') }}</strong></a>
+{{--                    <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'register']) }}"><strong class="text-danger">{{ __('Sign Up') }}</strong></a>--}}
                     <a class="site-header-item py-2 d-inline-block mr-3" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'login']) }}">{{ __('Sign In') }}</a>
                 @else
                     <a class="site-header-item py-2 d-inline-block mr-3" href="/logout">{{ __('Logout') }}</a>
@@ -43,36 +43,36 @@
     <footer class="py-5 bg-light">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md">
-                    <img src="{{ asset('images/logo.png', false) }}" height="45" />
-                    <small class="d-block mb-3 text-muted">&copy; {{ date('Y') }}</small>
-                    <small class="d-block text-muted">{{ __('Phones') }}:</small>
-                    <span class="text-muted"><a href="tel:7874183100">(787) 418-3100</a></span>
-                    <small class="d-block mt-3 text-muted">{{ __('Address') }}:</small>
-                    <span class="text-muted">1253 Ave. Fernandez Juncos, Esq. Roberto H. Todd, Pda. 18 Santurce, PR</span>
-
-                </div>
                 <div class="col-6 col-md">
-                    <h5>{{ __('Navigate') }}</h5>
                     <ul class="list-unstyled text-small">
                         <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'properties']) }}">{{ __('Properties') }}</a></li>
                         <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'contact']) }}">{{ __('Contact Us') }}</a></li>
                         @if (Auth::guest())
                             <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'register']) }}">{{ __('Sign Up') }}</a></li>
-                            <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'login']) }}">{{ __('Sign In') }}</a></li>
+{{--                            <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'login']) }}">{{ __('Sign In') }}</a></li>--}}
                         @else
                             <li><a class="text-muted" href="/logout">{{ __('Logout') }}</a></li>
                         @endif
 
+                        <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'privacy']) }}">{{ __('Privacy') }}</a></li>
+                        <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'terms']) }}">{{ __('Terms') }}</a></li>
+
                         <li><a class="text-muted" href="{{ route('backend.events.index') }}">{{ __('Administration') }}</a></li>
                     </ul>
                 </div>
+                <div class="col-6 col-md text-right">
+                    <small class="d-block text-muted">{{ __('Phones') }}:</small>
+                    <span class="text-muted"><a href="tel:7874183100">(787) 418-3100</a></span>
+                    <small class="d-block mt-3 text-muted">{{ __('Address') }}:</small>
+                    <span class="text-muted">1253 Ave. Fernandez Juncos, Esq. Roberto H. Todd, Pda. 18 Santurce, PR</span>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-6 col-md">
-                    <h5>{{ __('About') }}</h5>
-                    <ul class="list-unstyled text-small">
-                        <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'privacy']) }}">{{ __('Privacy') }}</a></li>
-                        <li><a class="text-muted" href="{{ route('frontend.page', ['locale' => App::getLocale(), 'pageSlug' => 'terms']) }}">{{ __('Terms') }}</a></li>
-                    </ul>
+                    <div class="main-logo-black"></div>
+                </div>
+                <div class="col-6 col-md text-right">
+                    <small class="d-block mb-3 text-muted">&copy; {{ date('Y') }}</small>
                 </div>
             </div>
         </div>
