d231d330d47f63ea42c03609034bb146c8cbdcd1
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2020-06-19 17:34:49 -0400

    Updates

--- resources/views/frontend/properties.blade.php
+++ resources/views/frontend/properties.blade.php
@@ -9,7 +9,7 @@
 
     <div class="properties-results bg-light-grey pt-4 pb-4">
         <div class="container">
-            <div class="alert alert-danger">{{__('New inventory in one hour')}}</div>
+{{--            <div class="alert alert-danger">{{__('New inventory in one hour')}}</div>--}}
 
             @if (session('success'))
                 <div class="alert alert-success">
