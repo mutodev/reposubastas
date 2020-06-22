810ff58d57eddc5ce12ada408e798aabc1a56589
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2020-06-22 17:21:09 -0400

    Reposubasta ga

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -39,6 +39,17 @@
           })(document);
         </script>
     @endif
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21501091-3"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-21501091-3');
+    </script>
+
 </head>
 <body>
 <div id="app" style="overflow: hidden;">
