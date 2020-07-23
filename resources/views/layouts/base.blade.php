e17cc5cc7f2eb38287a013ed3af4733ff6ceefbf
Eduardo Cosme <eduardo.cosme.h@gmail.com>
2020-07-23 13:40:15 -0400

    Updates

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -26,20 +26,6 @@
 
     @yield('stylesheets')
 
-    @if (!Auth::guest())
-    <!-- Smartsupp Live Chat script -->
-        <script type="text/javascript">
-          var _smartsupp = _smartsupp || {};
-          _smartsupp.key = '8e9639b7fc5dc2601f7712a1b673f4f4841faac1';
-          window.smartsupp||(function(d) {
-            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-            c.type='text/javascript';c.charset='utf-8';c.async=true;
-            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-          })(document);
-        </script>
-    @endif
-
 <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21501091-3"></script>
     <script>
--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -2,6 +2,20 @@
 
 @section('stylesheets')
     <link href="/css/frontend.css?v14" rel="stylesheet">
+
+    @if (!Auth::guest())
+    <!-- Smartsupp Live Chat script -->
+        <script type="text/javascript">
+          var _smartsupp = _smartsupp || {};
+          _smartsupp.key = '8e9639b7fc5dc2601f7712a1b673f4f4841faac1';
+          window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+          })(document);
+        </script>
+    @endif
 @endsection
 
 @section('main')
